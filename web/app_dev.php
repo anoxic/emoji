f2c1d4dfee3f2d175991870a8e85f680c7605e34
myklv <+@myklv.com>
2017-06-02 17:17:16 +0000

    add symfony/dotenv for credentials

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
@@ -21,6 +22,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
+
+$dotenv = new Dotenv();
+$dotenv->load(dirname(__DIR__).'/.env');
 
 $kernel = new AppKernel('dev', true);
 if (PHP_VERSION_ID < 70000) {
